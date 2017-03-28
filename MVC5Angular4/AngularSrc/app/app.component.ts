e020ead712605ff54dbecef3ffd2576179910988
EMKING <EMKING@DESKTOP-AKTCEF1>
2017-03-28 14:41:19 +0430

    Add project files.

---
+++ MVC5Angular4/AngularSrc/app/app.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  template: `<h1>Hello {{name}}</h1>`,
+})
+export class AppComponent  { name = 'Angular 4 from asp.net mvc 5'; }
---
+++ MVC5Angular4/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC5Angular4.MvcApplication" Language="C#" %>
